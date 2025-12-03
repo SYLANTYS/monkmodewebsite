ac8d8ee1a3b9f5344ba3606a69afe99e112e5275
Patrick Sylantys <92691815+SYLANTYS@users.noreply.github.com>
2025-12-03 14:21:52 -0500

    Revert download link to version 0.0.1

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -5,7 +5,7 @@ import { WindowsLogoIcon } from "@phosphor-icons/react";
 export default function DownloadButton({ scale }) {
   return (
     <a
-      href="https://github.com/SYLANTYS/monkmodewebsite/releases/download/v0.0.2/MonkMode-Setup-0.0.2.exe"
+      href="https://github.com/SYLANTYS/monkmodewebsite/releases/download/v0.0.1/MonkMode-Setup-0.0.1.exe"
       className={`flex items-center bg-blue-600 text-white rounded-lg px-4 py-2 space-x-1 hover:bg-blue-600/95 transition max-w-fit ${scale}`}
     >
       <WindowsLogoIcon size={28} weight="fill" />
